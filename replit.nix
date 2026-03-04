6cfb2f68ea55f587cbd09778d3829649aa3e9a99
wang <425845843@qq.com>
2026-03-04 15:43:51 +0800

    build: 更新 Node.js 版本至 20_x 并修改默认端口
    将 Node.js 依赖从 18_x 升级到 20_x 以使用最新稳定版本
    将默认端口从 8080 改为 3000 以符合常见开发实践

--- replit.nix
+++ replit.nix
@@ -1,15 +1,13 @@
 { pkgs }:
 
 pkgs.mkShell {
-  # 系统依赖
   buildInputs = [
-    pkgs.nodejs-18_x
+    pkgs.nodejs-20_x
     pkgs.npm
   ];
 
-  # 环境变量
   shellHook = ''
     export NODE_ENV="production"
-    export PORT="8080"
+    export PORT="3000"
   '';
 }
